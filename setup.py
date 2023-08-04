2fe723308536c03a455ab89081d983eaa7bd987b
erik-bach <67690715+erik-bach@users.noreply.github.com>
2023-08-03 23:52:04 -0700

    Add existing project files to Git

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting the average draft position of NBA players in fantasy basketball '
+                'using the previous season\'s statistics.',
+    author='Erik Bach',
+    license='MIT',
+)
